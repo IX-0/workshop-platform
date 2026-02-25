9177f88f0dcd10792c5406e7e2b91bea6da1a339
ixsb <igorbaltarejo05@gmail.com>
2026-02-25 17:53:13 +0000

    fix 2

--- platform/backend.tf
+++ platform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "workshop-ua-terraform-state-bucket-ixsb"
+    bucket         = "workshop-ua-dev-terraform-state-ixsb"
     key            = "terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-state-locks"
